7f12a5fab70645c13462e8572e56f623eab25921
Matthew Scholefield <matthew331199@gmail.com>
2019-01-25 22:02:40 -0600

    Increment version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='glokov',
-    version='0.1.0',
+    version='0.1.1',
     description='A text generation library',
     url='http://github.com/MatthewScholefield/glokov',
     author='Matthew Scholefield',
